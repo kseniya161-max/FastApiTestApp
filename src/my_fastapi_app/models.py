e41afca813842a79c480487e9964c1bb099f7ca8
Kseniya <baharevaxen@yandex.ru>
2025-12-09 21:30:36 +0300

    model operator correcting

--- src/my_fastapi_app/models.py
+++ src/my_fastapi_app/models.py
@@ -7,15 +7,16 @@ from src.my_fastapi_app.database import Base
 class Operator(Base):
     __tablename__='operators'
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(Integer,prymary_key=True, index=True)
-    is_active = Column(Boolean, defalt=True)
-    max_active_lead = Column(Integer, default=5)
+    name = Column(String, index=True)
+    is_active = Column(Boolean, default=True)
+    max_active_leads = Column(Integer, default=5)
     weight = Column(Integer, default=1)
 
     leads = relationship("Lead", back_populates="operator")
 
 
 class Lead(Base):
+    __tablename__ = 'leads'
     id = Column(Integer, primary_key=True, index=True)
     external_id = Column(String, unique=True, index=True)
     operator_id = Column(Integer, ForeignKey("operators.id"))
