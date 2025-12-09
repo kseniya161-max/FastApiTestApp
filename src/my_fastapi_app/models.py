ae1c274b60446965cdbc68251244021a690fbe0b
Kseniya <baharevaxen@yandex.ru>
2025-12-09 21:37:20 +0300

    create models Source and Contact

--- src/my_fastapi_app/models.py
+++ src/my_fastapi_app/models.py
@@ -23,3 +23,35 @@ class Lead(Base):
     operator = relationship("Operator", back_populates="leads")
 
 
+class Source(Base):
+    __tablename__= 'sources'
+    name = Column(String, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    operator_weights = relationship("SourceWeight", back_populates="source")
+
+
+class Contact(Base):
+    __tablename__ = 'contacts'
+    id = Column(Integer, primary_key=True, index=True)
+    lead_id = Column(Integer, ForeignKey("leads.id"))
+    source_id = Column(Integer, ForeignKey("sources.id"))
+    operator_id = Column(Integer, ForeignKey("operators.id"))
+
+    lead = relationship("Lead", back_populates="contacts")
+    source = relationship("Source")
+    operator = relationship("Operator")
+
+
+class SourceWeight(Base):
+    __tablename__ = 'source_weights'
+    id = Column(Integer, primary_key=True, index=True)
+    source_id = Column(Integer, ForeignKey("sources.id"))
+    operator_id = Column(Integer, ForeignKey("operators.id"))
+    weight = Column(Integer)
+
+    source = relationship("Source", back_populates="operator_weights")
+    operator = relationship("Operator", back_populates="source_weights")
+
+
+
+
